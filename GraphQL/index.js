1fdcc177952ef55c4823b30ecd6d817375717f47
Asyraf Norafandi <asyrafnorafandi@gmail.com>
2019-06-24 15:45:34 +0800

    Initialize prisma and graphql

---
+++ GraphQL/index.js
@@ -0,0 +1,12 @@
+const resolvers =  require('./resolvers');
+const { prisma } =  require('../prisma/prisma-client');
+const { GraphQLServer } =  require('graphql-yoga');
+
+const server = new GraphQLServer({
+  typeDefs: './GraphQL/schema.graphql',
+  resolvers,
+  context: {
+    prisma,
+  },
+});
+server.start(() => console.log('Server is running on http://localhost:4000'));
---
+++ GraphQL/resolvers/index.js
@@ -0,0 +1,13 @@
+const Query = require('./Query');
+const Mutation = require('./Mutation');
+const Post = require('./Post');
+const User = require('./User');
+
+const resolvers = {
+  Query,
+  Mutation,
+  Post,
+  User,
+};
+
+module.export = resolvers;
